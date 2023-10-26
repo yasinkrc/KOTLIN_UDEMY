4766309d38f9250d667e5d390632388884fc4d15
yasinkrc <113844850+yasinkrc@users.noreply.github.com>
2023-10-26 22:27:53 +0300

    Add files via upload

---
+++ degiskenler_ve_veritipleri/Degisken_Olusturma.kt
@@ -0,0 +1,16 @@
+package com.example.degiskenler_ve_veritipleri
+
+fun main(){
+
+
+    var name ="Yasin"
+    var age=22
+    var height=153.4f
+    var harf='Y'
+
+    println(name)
+    println(age)
+    println(height)
+    println(harf)
+}
+
